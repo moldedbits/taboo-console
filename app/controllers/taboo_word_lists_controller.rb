ed7ef65d80f574a3509606b2054a701ddf91b652
bittu-moldedbits <bittu@moldedbits.com>
2015-09-25 19:13:57 +0530

    Finished taboo APIs after minor changes in UI

--- app/controllers/taboo_word_lists_controller.rb
+++ app/controllers/taboo_word_lists_controller.rb
@@ -18,30 +18,29 @@ class TabooWordListsController < ApplicationController
     uploaded_file_path = params[:file].path
     word_lists = CSV.read(uploaded_file_path)
     @message = ""
-    @all_error_rows = "" #new
+    @all_error_rows = ""
     @new_word_count = 0
     @words_with_error = Array.new
     @repeated_words = Array.new
     @repeated_words = word_lists.collect {|x| x[0]}.select{|e| word_lists.collect{|x| x[0]}.count(e) > 1}.uniq
     word_lists.each_with_index do |word,i|
-      @new_taboo_word = TabooWordList.new()
-      @new_taboo_word.word = word[0]
-      @new_taboo_word.taboo_word_1 = word[1]
-      @new_taboo_word.taboo_word_2 = word[2]
-      @new_taboo_word.taboo_word_3 = word[3]
-      @new_taboo_word.taboo_word_4 = word[4]
-      @new_taboo_word.taboo_word_5 = word[5]
-      @new_taboo_word.version_number = -1
+      new_taboo_word = TabooWordList.new()
+      new_taboo_word.word = word[0]
+      new_taboo_word.taboo_word_1 = word[1]
+      new_taboo_word.taboo_word_2 = word[2]
+      new_taboo_word.taboo_word_3 = word[3]
+      new_taboo_word.taboo_word_4 = word[4]
+      new_taboo_word.taboo_word_5 = word[5]
+      new_taboo_word.version_number = -1
       if word.length != 6
         @words_with_error << i + 1
         next
-      elsif @new_taboo_word.save
+      elsif new_taboo_word.save
           @new_word_count = @new_word_count + 1
       end
     end
-    puts "aifsduhfbasduhvbsduvbv " + word_lists.size.to_s + @words_with_error.size.to_s
-    if @words_with_error.size == word_lists.size #new
-      @all_error_rows = "- All rows in this file contains errors" #new
+    if @words_with_error.size == word_lists.size
+      @all_error_rows = "- All rows in this file contains errors"
     elsif @new_word_count == 0
         @message = " - All words are already present in database"
     end
--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module TabooApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.active_record.raise_in_transactional_callbacks = true
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
